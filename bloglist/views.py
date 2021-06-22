503e8cb21db9bca1d57562d2bcabd020de9705a7
davnardin <85520967+davnardin@users.noreply.github.com>
2021-06-22 14:55:26 +0200

    Update views.py

--- bloglist/views.py
+++ bloglist/views.py
@@ -14,30 +14,4 @@ def prova(request):
     object_list = Post.objects.filter(categoria__descrizione='google-ads')
     return render(request, 'blog/prova.html', {'posts': object_list})
 
-"""
-FUNZIONI DAL LIBRO DJANGO BY EXAMPLE (BLOG APPLICATION)
 
-def post_list(request, tag_slug=None):    
-    object_list = Post.published.all()
-    tag = None
-    
-
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
-
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': object_list, 'tag': tag})
-
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
-"""
